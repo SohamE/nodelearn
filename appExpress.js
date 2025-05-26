ae0c10159cee9e6cd831b7b2f001604caa95eeec
SohamE <soham.584sengupta@gmail.com>
2025-05-26 22:47:10 +0530

    Post data

--- appExpress.js
+++ appExpress.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 // app.get("/", myController.home);
 // app.get("/", (req, res) => {
 //   res.status(404).json("Not found");
@@ -22,6 +23,20 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body};
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({ 
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+     })
+  })
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}`);
